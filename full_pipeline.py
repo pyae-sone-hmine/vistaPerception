d756eeec2b92847d0fd48d7ffc14a0ba0e46e0fc
Hector Martinez <hectorxm@mit.edu>
2024-02-01 17:14:12 -0500

    full_pipeline

--- full_pipeline.py
+++ full_pipeline.py
@@ -8,7 +8,9 @@ from vista.utils import transform
 from vista.entities.agents.Dynamics import tireangle2curvature
 
 def main(args):
-    world = vista.World(args.trace_path, trace_config={'road_width': 4})
+    # CREATE A VISTA WORLD FOR OUR SIMULATION
+    world = vista.World(args.trace_path, trace_config={'road_width': 4}) 
+    # POPULATE THE WORLD WITH A CAR WITH PERHAPS OTHER CONFIGS
     car = world.spawn_agent(
         config={
             'length': 5.,
@@ -18,11 +20,17 @@ def main(args):
             'lookahead_road': True
         })
 
+
+    # PERHAPS GET RID OF THE CAMERA? 
     camera_front = car.spawn_camera(config={
         'size': (200, 320),
     }) # another problem here, since I will likely need a camera object
+
+    # no need to display, but will be there in case we want to run locally
     display = vista.Display(world)
     
+
+    # for analysis after the fact
     has_video_writer = args.out_path is not None
     if has_video_writer:
         os.makedirs(os.path.dirname(args.out_path), exist_ok=True)
@@ -30,19 +38,36 @@ def main(args):
         # fourcc = cv2.VideoWriter_fourcc(*'XVID')
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         video_writer = cv2.VideoWriter(args.out_path, fourcc, 30, (960, 320))
-    
+
+
+    # this is giving us issues, but is necessary to spawn agents and update traces
     world.reset()
     display.reset()
 
+
+    # actually running simulated movement of agent
     frame_idx = 0
-    while not car.done and frame_idx <= 300:
+    while not car.done and frame_idx <= 300: # can change this for longer simuls
         try:
             print(f"Frame Index: {frame_idx}")
-            # action = follow_human_trajectory(car) # FIXME changed this
-            action = np.array([0,10]) # FIXME this is a hard-coded statespace controller
+
+
+            ### PYAE'S TODO 
+
+            """
+            will have action = (output_of_BarrierNet, given Hector's perception input and BarrierNet processing, 
+            will need segmentation and depth), 
+            syntax for action is 2-tuple with (angle, velocity), so make sure to format it correctly
+            how would you fetch the correct format of the BarrierNet output (formatted as (angle, velocity))
+            """
+
+
+            ### END PYAE'S TODO
+            action = None # REMOVE THIS AFTER
             car.step_dynamics(action)
             car.step_sensors()
 
+            # this is for the irrelevant display, we can ignore but keep it 
             vis_img = display.render()
             if has_video_writer:
                 video_writer.write(vis_img[:, :, ::-1])
